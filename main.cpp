de9723fd70a774631461d3a8e044677e006204db
Vinícius Abner <44265900+v1n1c1u@users.noreply.github.com>
2019-09-11 19:49:12 -0300

    Ajustes maneiros
    Espero que não tenha feito caca

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 
 /* Animais da ao serem atravessados pelo rio */
-int galinha = 1;
-int raposa = 2;
-int cachorro = 3;
+int const galinha = 1;
+int const raposa = 2;
+int const cachorro = 3;
 
 /* Opcao selecionada*/
 int opcao;
@@ -32,35 +32,53 @@ int margem2[3] = {};
 
 /* Boolean para verificar se existe mais de um animal na margem */
 bool maisDeUmAnimalNaMargem(int m[]){
-    if (m[0] == 0 and m[2] == 0){
-        return false;
-    }
-    else if (m[1] == 0 and m[2] == 0){
-        return false;
-    }
-    else if (m[0] == 0 and m[1] == 0){
-        return false;
-    }
+    int animais = 0;
+	for (int i = 0; i < 3; i++) {
+		if (m[i] > 0) {
+			animais++;
+		}
+	}
+	if (animais <= 1) {
+		return false;
+	}
     return true;
 }
 
-/* Calcula a soma dos animais da margem escolhida */
+/* Calcula a soma dos animais da margem escolhida
 int calculaMargem(int m[]){
     int soma = 0;
     for (int i = 0; i < 3; i++){
         soma += m[i];
     }
     return soma;
+}*/
+
+/*verifica se ha conflitos de animais na margem. Achei mais intuitivo que o calculaMargem, mas ai fica a criterios de vcs*/
+bool haConflitos(int m[]) {
+	if (m[0] != 0 and m[1] != 0 and m[2] == 0) {
+		return true;
+	}
+	else if (m[0] == 0 and m[1] != 0 and m[2] != 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+/*verifica se um array ta cheio (usado pra ver se a margem2 esta completa e o cara ganhou o jogo) */
+bool ganhou(int m[]) {
+      if(m[0]!=0 and m[1]!=0 and m[2]!=0){
+	return true;
+      }
+	return false;
 }
 
 /* Verifica se certo animal existe em determinada margem */
 bool verificaMargem(int m[], int animal){
-    for (int i = 0; i < 3; i++){
-        if (m[i] == animal){
-            return true;
-        }
+    if(m[animal-1]!=0){
+	return true;    
     }
-    return false;
+	return false;
 }
 /* colocando temporariamente o que teve na jogada em arrays terceiros */
 void tempMargem(int margem1[], int margem2[], int temp1margem[], int temp2margem[]){
@@ -118,17 +136,17 @@ void travessia(int m1[], int m2[], int animal){
 
 /* Confirma se o jogador perdeu ou ganhou o jogo */
 bool situacaoJogador(int m1[], int m2[]){
-    if (calculaMargem(m1)%2!= 0 and maisDeUmAnimalNaMargem(m1) and barqueiro == false){
+    if (!haConflitos() and maisDeUmAnimalNaMargem(m1) and barqueiro == false){
         cout << " " << endl;
         cout << "                                      Ｐｅｒｄｅｕ ！        " << endl;
         return false;
     }
-    if (calculaMargem(m2)%2!=0 and maisDeUmAnimalNaMargem(m2) and barqueiro == true){
+    if (!haConflitos() and maisDeUmAnimalNaMargem(m2) and barqueiro == true){
         cout << " " << endl;
         cout << "                                      Ｐｅｒｄｅｕ ！        " << endl;
         return false;
     }
-    if (calculaMargem(m2) == 6){
+    if (ganhou()){
         cout << " " << endl;
         cout << "                             Ｇａｎｈｏｕ！ Ｐａｒａｂｅｎｓ ！        " << endl;
         return false;
@@ -187,7 +205,7 @@ void desfazerJogada(int margem1[], int margem2[], int margem1copy[], int margem2
     if (barqueiro == false){
         barqueiro = true;
     }
-    else if (barqueiro == true){
+    else{
         barqueiro = false;
     }
     travessia(margem1, margem2, 0);
