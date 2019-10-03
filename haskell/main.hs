9a9457f6fa8cdf1b644a076ce504f709fd2bbc5f
Vinícius Abner <44265900+v1n1c1u@users.noreply.github.com>
2019-10-03 19:12:52 -0300

    Esqueleto
    Um esboço muito mal feito do programa

---
+++ haskell/main.hs
@@ -0,0 +1,16 @@
+--definição das constantes
+barqueiro :: Int
+	barqueiro = 0
+galinha :: Int
+	galinha = 1
+raposa :: Int
+	raposa = 2
+cachorro ::Int
+	cachorro = 3
+
+--pensei que seria melhor uma função só no main, e dentro dela fazer tudo.
+-- ela seria o while, mas ainda não pensei como fazer a recursão e condição de parada
+iniciar :: IO()
+
+main = do
+	iniciar
