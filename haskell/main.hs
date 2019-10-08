a0453905ab36acded0a46b9f8728da2b4cf09979
Vinícius Abner <44265900+v1n1c1u@users.noreply.github.com>
2019-10-08 08:07:15 -0300

    Update
    Um adianto que eu fiz

--- haskell/main.hs
+++ haskell/main.hs
@@ -8,9 +8,23 @@ raposa :: Int
 cachorro ::Int
 	cachorro = 3
 
---pensei que seria melhor uma função só no main, e dentro dela fazer tudo.
--- ela seria o while, mas ainda não pensei como fazer a recursão e condição de parada
-iniciar :: IO()
+situacaoDoJogador :: [Int]->[Int]->Bool
+situacaoDoJogador margem1 margem2
+	| margem1==[0,raposa,cachorro] || margem2==[0,raposa,cachorro] = False -- perdeu
+	| margem1==[galinha,raposa,0] || margem2==[galinha,raposa,0] = False --perdeu
+	|otherwise = True --Ainda jogando 
+
+ganhou :: [Int]->Bool
+ganhou margem2 
+	|margem2 == [1,2,3] = True
+	|otherwise = False
+
+-- Essa implementção aí não tá ideal pra função de iniciar, mas é um começo
+iniciar ([Int]->[Int]=>Bool)->([Int]=>Bool)->IO()
+iniciar situacao ganhou = 
+		if situacao == True && ganhou == False then =
+		else if situacao == False && ganhou == False then = putStrLn "Perdeu!"
+		else = putStrLn "Perdeu!"
 
 main = do
-	iniciar
+	iniciar (situacaoDoJogador [1,2,3] [0,0,0]) (ganhou [0,0,0])
