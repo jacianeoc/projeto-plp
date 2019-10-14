c883573b9cfb3caf756c75b778eadc7de4741b21
Vinícius Abner <44265900+v1n1c1u@users.noreply.github.com>
2019-10-14 08:54:41 -0300

    Quase lá
    Tirei os emojis e resolvi os problemas de compilação que apareceram, menos o com a função 'ganhou'. O compilador diz que eu tô usando ela da maneira errada e eu não sei pq. Resolvendo esse, acho que o programa roda de boa e só falta melhorar as saídas de texto

--- haskell/main.hs
+++ haskell/main.hs
@@ -8,49 +8,63 @@ cachorro = 3
 barqueiro :: Int
 barqueiro = 4
 --Cada margem é uma quadrupla (galinha,raposa,cachorro,barqueiro)
-data Margem = Quadruple Int Int Int Int deriving(show, eq)
+data Margem = Quadruple Int Int Int Int deriving(Show,Eq)
 
 situacaoDoJogador :: Margem -> Margem -> Bool
-situacaoDoJogador m1 m2 =
-	| m1==(Quadruple 0 raposa cachorro 0) || m2 == (Quadruple 0 raposa cachorro 0) = False --perdeu
-	| m1==(Quadruple galinha raposa 0 0) || m2 == (Quadruple galinha raposa 0 0) = False --perdeu
-	| otherwise = True --Ainda jogando 
-
-ganhou :: Margem->Bool
-ganhou  (Quadruple galinha,raposa,cachorro) = True
-ganhou _ = False
+situacaoDoJogador m1 m2 
+	|m1 == (Quadruple 0 raposa cachorro 0) || m2 == (Quadruple 0 raposa cachorro 0) = False --perdeu
+	|m1 == (Quadruple galinha raposa 0 0) || m2 == (Quadruple galinha raposa 0 0) = False --perdeu
+	|otherwise = True --Ainda jogando 
+	
+--Recebe a margem2 e verifica se ela tem todos os animais e o barqueiro
+ganhou :: Margem -> Bool
+ganhou m2
+	|m2 == (Quadruple galinha raposa cachorro barqueiro) = True
+	|otherwise = False
 
 fazerJogada :: Margem -> Margem ->Int-> (Margem,Margem)
 fazerJogada (Quadruple a b c 4) (Quadruple e f g 0) opcao
-	| opcao == 1 = ((Quadruple 0 b c 0 ), (Quadruple 1 f g 4))
-	| opcao == 2 = ((Quadruple a 0 c 0 ), (Quadruple e 2 g 4))
-	| opcao == 3 = ((Quadruple a b 0 0 ), (Quadruple e f 3 4))
-	| opcao == 4 = ((Quadruple a b c 0 ), (Quadruple e f g 4))
-	| otherwise = ((Quadruple a b c 4), (Quadruple e f g 0))
+	|opcao == 1 = ((Quadruple 0 b c 0 ), (Quadruple 1 f g 4))
+	|opcao == 2 = ((Quadruple a 0 c 0 ), (Quadruple e 2 g 4))
+	|opcao == 3 = ((Quadruple a b 0 0 ), (Quadruple e f 3 4))
+	|opcao == 4 = ((Quadruple a b c 0 ), (Quadruple e f g 4))
+	|otherwise = ((Quadruple a b c 4), (Quadruple e f g 0))
 fazerJogada (Quadruple a b c 0) (Quadruple e f g 4) opcao
-	| opcao == 1 = ((Quadruple 1 b c 4 ), (Quadruple 0 f g 0))
-	| opcao == 2 = ((Quadruple a 2 c 4 ), (Quadruple e 0 g 0))
-	| opcao == 3 = ((Quadruple a b 3 4 ), (Quadruple e f 0 0))
-	| opcao == 4 = ((Quadruple a b c 4 ), (Quadruple e f g 0))
-	| otherwise = ((Quadruple a b c 0), (Quadruple e f g 4))
+	|opcao == 1 = ((Quadruple 1 b c 4 ), (Quadruple 0 f g 0))
+	|opcao == 2 = ((Quadruple a 2 c 4 ), (Quadruple e 0 g 0))
+	|opcao == 3 = ((Quadruple a b 3 4 ), (Quadruple e f 0 0))
+	|opcao == 4 = ((Quadruple a b c 4 ), (Quadruple e f g 0))
+	|otherwise = ((Quadruple a b c 0), (Quadruple e f g 4))
 
+first :: Margem -> Int
+first (Quadruple a b c d) = a
+
+second :: Margem -> Int
+second (Quadruple a b c d) = b
+
+third :: Margem -> Int
+third (Quadruple a b c d) = c
+
+fourth :: Margem -> Int
+fourth (Quadruple a b c d) = d
+
+--Imprime as duas margens baseado na margem1
 visualizaMargens::Margem -> IO()
 visualizaMargens m1 = do
 	putStrLn ""
 	putStrLn "                                   Ｔｒａｖｅｓｓｉａ" 
-	if m1 == (Quadruple 1 _ _ _) then putStrLn " 1- Galinha   -->        🐥░                       ░       "
-	else putStrLn " 1- Galinha   -->        ░                       ░🐥       "
-	if m1 == (Quadruple _ 2 _ _) then putStrLn " 2- Raposa    -->       🦊 ░                       ░       "
-	else putStrLn " 2- Raposa    -->       ░                       ░🦊       "
-	if m1 == (Quadruple _ _ 3 _) then putStrLn " 3- Cachorro  -->       🐶 ░                       ░       " 
-	else putStrLn " 3- Cachorro  -->        ░                       ░🐶       "
-	if m1 == (Quadruple _ _ _ 4) then putStrLn " 4- Barqueiro -->          ░⛵                   ░       "
-	else putStrLn " 4- Barqueiro -->        ░                     ⛵░       " 
+	if (first m1) == 1 then putStrLn " 1- Galinha ||                      ||       "
+	else putStrLn " 1-         ||                       ||Galinha       "
+	if (second m1) == 2 then putStrLn " 2- Raposa ||                      ||       "
+	else putStrLn " 2- ||                      ||Raposa       "
+	if (third m1) == 3 then putStrLn " 3- Cachorro ||                      ||       "
+	else putStrLn " 3-  ||                      ||Cachorro       "
+	if (fourth m1) == 4 then putStrLn " 4- Barqueiro ||                      ||       "
+	else putStrLn " 4-  ||                      ||Barqueiro       "
 	
 desfazerJogada :: (Margem,Margem) -> (Margem,Margem) -> Char -> (Margem,Margem)
-desfazerJogada a b opcao =
+desfazerJogada a b opcao 
 	|opcao=='s' = a
-	|opcao=='n' = b
 	|otherwise = b
 	
 mostrarSolucao :: Char -> IO()
@@ -75,7 +89,7 @@ iniciar _ _ False _ = do
 	putStrLn "Mostrar solução? [s/n]"
 	opcao <- getChar
 	mostrarSolucao opcao
-iniciar _ _ _ True =  putStrLn "                             Ｇａｎｈｏｕ！ Ｐａｒａｂｅｎｓ ！        "
+iniciar _ _ _ True =  do putStrLn "                             Ｇａｎｈｏｕ！ Ｐａｒａｂｅｎｓ ！        "
 iniciar m1 m2 situacao ganhou = do
 	visualizaMargens m1
 	putStrLn ""
@@ -84,20 +98,16 @@ iniciar m1 m2 situacao ganhou = do
 
 	let r = fazerJogada m1 m2 opcao
 	visualizaMargens (fst r)
-	if (m1==(fst r) and m2==(snd r)) then 
+	if (m1==(fst r) && m2==(snd r)) then do
 		putStrLn ""
 		putStrLn "Animal selecionado nao se encontra nessa margem"
 		iniciar (fst r) (snd r) (situacaoDoJogador (fst r) (snd r)) (ganhou (snd r))
-	else putStrLn "" 
+	else do putStrLn "" 
 		putStrLn "Desfazer Jogada? [s/n] "
 		opcao <- getChar
 		let d = desfazerJogada (m1,m2) r opcao
 		iniciar (fst d) (snd d) (situacaoDoJogador (fst d) (snd d)) (ganhou (snd r))
 		
-	-- Essa chamada recursiva aqui em baixo acho que não precisava, 
-	-- mas deixei comentada por garantia. Colei ela no primeiro if
-      --iniciar (fst r) (snd r) (situacaoDoJogador (fst r) (snd r)) (ganhou (snd r))
-
 main = do
 	putStrLn "BEM VINDO A TRAVESSIA!"
 	putStrLn ""
@@ -111,5 +121,4 @@ main = do
 	putStrLn "5° - As outras combinações de animais não apresentam o problema de um atacar o outro."
 	putStrLn ""
 	
-	iniciar (Quadruple 1 2 3 4) (Quadruple 0 0 0 0) (situacaoDoJogador (Quadruple 1 2 3 4)) (Quadruple 0 0 0 0)) (ganhou (Quadruple 0 0 0 0))
-
+	iniciar (Quadruple 1 2 3 4) (Quadruple 0 0 0 0) (situacaoDoJogador (Quadruple 1 2 3 4) (Quadruple 0 0 0 0)) (ganhou (Quadruple 0 0 0 0))
